7275fe217d3ed2bb300c2fc4e30aae9ef7e3b472
fightyz <yangzhou1989@gmail.com>
2014-05-15 15:28:09 +0800

    can be encode rightly

--- Challenge#162/easy.cpp
+++ Challenge#162/easy.cpp
@@ -1,90 +1,65 @@
-// http://www.reddit.com/r/dailyprogrammer/comments/25clki/5122014_challenge_162_easy_novel_compression_pt_1/
-
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
-int main(){
-	int n, idx;
-	int word_flag, capitalised_flag, upper_case_flag, hyphen_flag, times, newline_flag, symbol_flag;
-	vector<string> dict;
-	string word, str;
+int main() {
+	ifstream in("input.in");
+	streambuf *cinbuf = cin.rdbuf();
+	cin.rdbuf(in.rdbuf());
 
-	cin >> n;
+	ofstream out("output.out");
+	streambuf *coutbuf = cout.rdbuf();
+	cout.rdbuf(out.rdbuf());
+  /* Load dictionary. */
+  vector<string> dictionary;
+  unsigned count;
+  cin >> count;
+  while (count-- > 0) {
+    string word;
+    cin >> word;
+    dictionary.push_back(word);
+  }
 
-	for(int i = 0; i < n; i++){
-		cin >> word;
-		dict.push_back(word);
-	}
-	word_flag = 0;
-	capitalised_flag = 0;
-	upper_case_flag = 0;
-	hyphen_flag = 0;
-	times = 0;
-	newline_flag = 0;
-	symbol_flag = 0;
+  /* Decompress input */
+  bool needSpace = false;
+  string token;
+  while (!cin.eof()) {
+    cin >> token;
+    if (token == "E") {
+      break;
+    } else if (token == "R") {
+      cout << endl;
+      needSpace = false;
+    } else if (token == "-") {
+      cout << "-";
+      needSpace = false;
+    } else if (token.find_first_of(".,?!;:") == 0) {
+      cout << token;
+      needSpace = true;
+    } else {
+      if (needSpace) cout << " ";
+      string word = dictionary[stoi(token)];
+      char modifier = token[token.size() - 1];
+      switch (modifier) {
+        case '^':
+          word[0] = toupper(word[0]);
+          break;
+        case '!':
+          for (auto &c : word) {
+            c = toupper(c);
+          }
+          break;
+      }
+      cout << word;
+      needSpace = true;
+    }
+  }
+  cin.rdbuf(cinbuf);
+  cout.rdbuf(coutbuf);
 
-	while(!cin.eof()){
-		if(newline_flag){
-			newline_flag = 0;
-			times = 0;
-			cout << endl;
-		}
-		
-		cin >> str;
-		idx = 0;
-		for(string::size_type i = 0; i < str.size(); i++){
-			if(str[i] >= '0' && str[i] <= '9'){//this string is a word
-				idx = idx * 10 + str[i] - '0';
-				word_flag = 1;
-			}else if(str[i] == '^'){
-				capitalised_flag = 1;
-			}else if(str[i] == '!' && i != 0){
-				upper_case_flag = 1;
-			}else if(str[i] == '-'){
-				hyphen_flag = 2;
-				cout << str[i];
-			}else if(str[i] == '.' || str[i] == ',' || str[i] == '?' 
-				|| str[i] == '!' || str[i] == ';' || str[i] == ':'){
-				symbol_flag = 1;
-				cout << str[i];
-			}else if(str[i] == 'R' || str[i] == 'r'){
-				newline_flag = 1;
-			}else if(str[i] == 'E' || str[i] == 'e'){
-				return 0;
-			}
-		}
-
-		if(symbol_flag){
-			symbol_flag = 0;
-			continue;
-		}
-		if(newline_flag){
-			continue;
-		}
-
-		if(hyphen_flag <= 0 && times){
-			cout << " ";
-		}else{
-			hyphen_flag--;
-		}
-
-		if(word_flag){
-			word_flag = 0;
-			string tmp = dict[idx];
-			if(capitalised_flag){
-				capitalised_flag = 0;
-				tmp[0] = tmp[0] - 32;
-			}else if(upper_case_flag){
-				upper_case_flag = 0;
-				for(string::size_type k = 0; k < tmp.size(); k++){
-					tmp[k] = tmp[k] - 32;
-				}
-			}
-			cout << tmp;
-		}
-		times++;
-	}
+  return 0;
 }
